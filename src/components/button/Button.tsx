8dcbc777b1274b1ef4cd9bf4b349169a502693a1
nguyenyou <tunt081295@gmail.com>
2025-12-02 19:57:14 +0700

    🚢 ship it

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,5 +3,5 @@ interface ButtonProps {
 }
 
 export const Button = ({ label }: ButtonProps) => {
-  return <button>{label}</button>
+  return <button className="h-32 rounded-4 px-12 py-6 text-gray-0 bg-primary-4 hover:bg-primary-3 active:bg-primary-5">{label}</button>
 }
