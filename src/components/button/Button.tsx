15b7c24d68ccf50ae131bd6d45e88128eabf60c6
nguyenyou <tunt081295@gmail.com>
2025-12-03 00:38:16 +0700

    🚢 ship it

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,7 +1,23 @@
+import { cn } from "../../lib/utils";
+import { cva, type VariantProps } from "class-variance-authority";
+
 interface ButtonProps {
-  label: string
+  label: string;
 }
 
-export const Button = ({ label }: ButtonProps) => {
-  return <button className="h-32 rounded-4 px-12 py-6 text-gray-0 bg-primary-4 hover:bg-primary-3 active:bg-primary-5">{label}</button>
-}
+const buttonVariants = cva(
+  "h-32 rounded-4 px-12 py-6 text-gray-0 bg-primary-4 hover:bg-primary-3 active:bg-primary-5",
+  {
+    variants: {
+      variant: {
+        outline: "border",
+      },
+    },
+  }
+);
+
+export const Button = ({
+  label,
+}: ButtonProps & VariantProps<typeof buttonVariants>) => {
+  return <button className={cn(buttonVariants())}>{label}</button>;
+};
